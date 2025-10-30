d61e47f0f2d1bcfb0f4413148260081c07584b5b
dar-ienn <amadarien23@gmail.com>
2025-10-30 13:52:23 -0700

    Initial commit - AiGrader Spring Boot base setup

---
+++ src/main/java/com/example/aigrader/AiGraderApplication.java
@@ -0,0 +1,13 @@
+package com.example.aigrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGraderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiGraderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aigrader/AiGraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aigrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGraderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
